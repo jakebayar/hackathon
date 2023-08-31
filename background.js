7ef7e483c35a2a651fb74b6377769533d5412de9
noname <noname@nonames-MacBook-Pro.local>
2023-08-31 09:55:57 -0400

    update

--- background.js
+++ background.js
@@ -1,20 +1,28 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
+    function (details, callback) {
         const url = new URL(details.url);
         const keyword = url.searchParams.get('q');
 
-        return new Promise((resolve) => {
-            chrome.storage.local.get(keyword, function (result) {
-                if (result[keyword]) {
-                    resolve({ redirectUrl: result[keyword] });
+        // Check if the keyword starts with "go "
+        if (keyword.startsWith('go ')) {
+            const actualKeyword = keyword.substring(3);  // remove "go "
+
+            chrome.storage.local.get('shortlinks', function (data) {
+                const shortlinks = data.shortlinks || {};
+
+                if (shortlinks[actualKeyword]) {
+                    callback({ redirectUrl: shortlinks[actualKeyword] });
                 } else {
                     // If the keyword is not found, you can decide what to do. 
-                    // For instance, redirect to a default page or show an error.
-                    resolve({ redirectUrl: "https://defaultpage.com" });
+                    callback({ redirectUrl: "https://defaultpage.com" });
                 }
             });
-        });
+        } else {
+            callback({});
+        }
+
     },
-    { urls: ["https://colinks-placeholder.com?q=*"] },
+    { urls: ["https://www.google.com/search?q=go%20*"] },  // adjust this pattern as per your setup
     ["blocking"]
 );
+
